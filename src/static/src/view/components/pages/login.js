f6a6119fa2af4841c32e906298869e8c9d609ae8
maureenv <maureenvogel@outlook.com>
2018-09-16 14:05:44 -0400

    Try to use fetch for request

--- src/static/src/view/components/pages/login.js
+++ src/static/src/view/components/pages/login.js
@@ -4,17 +4,35 @@ import { NavLink } from 'react-router-dom'
 
 class Login extends Component {
   state = {
-    //showMenus: false,
+    password: '',
+    email: '',
   }
-  render() {
-    return (
-      <form id="login-form" action="/login" method="post">
-        <div> Log In </div>
-        <label> Email </label><input type="text" id="email" name="email"/>
-        <label> Password </label><input type="password" id="password" name="password"/>
 
-        <input type="submit" id="submit-button"/>
-      </form>
+  submit = e => {
+    e.preventDefault()
+
+    const data = this.state
+    console.log(data, 'the data')
+    fetch('/login', {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers(),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data, 'the data in response'))
+    .catch((err)=> console.log(err, 'the error'))
+  }
+
+  render() {
+          /*<form id="login-form" action="/login" method="post">*/
+    return (
+      <div>
+        <div> Log In </div>
+        <label> Email </label><input type="text" id="email" onChange={ e => this.setState({ email: e.target.value })} name="email"/>
+        <label> Password </label><input type="password" id="password" onChange={ e => this.setState({ password: e.target.value })} name="password"/>
+
+        <button onClick={ this.submit }>Submit</button>
+      </div>
     )
   }
 }
